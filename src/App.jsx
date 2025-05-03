0bf1c03efe4aa137229c23f1429f88e04dda8fb4
Tasha McGlashan <tmcglashan77@gmail.com>
2025-05-03 00:43:41 -0700

    added src, vite-project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='text-5xl text-orange-500 font-bold'>Welcome to Awwwards</h1>
+    </main>
+
+  )
+}
+
+export default App
